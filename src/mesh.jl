be867ef73a1fb44b3e2aa4ed3bcc6ab7180185e4
Amani Toussaint <amanit@mit.edu>
2023-05-13 00:53:26 -0400

    connectivity for sphere query+normalize triangle area to 1

--- src/mesh.jl
+++ src/mesh.jl
@@ -1,3 +1,5 @@
+using DataStructures
+
 mutable struct Mesh
     V::Matrix{Float64}  # 3×|V|
     F::Matrix{Int} # 3×|F|
@@ -37,6 +39,47 @@ function get_in_sphere(mesh::Mesh, point, radius)
     V_sampled, F_sampled
 end
 
+function connected_mesh_in_sphere(mesh::Mesh, point, radius)
+    V_sampled, F_sampled = get_in_sphere(mesh, point, radius)
+
+    dist = vec(norm(mesh.V .- point, dims=1))
+    start_vertex = argmin(dist)
+    
+    visited_vertices = .!V_sampled
+    stack = Stack(Int)
+    push!(stack, start_vertex)
+    visited_vertices[start_vertex] = true
+    visited_faces = .!F_sampled
+
+    # DFS for reachability
+    while !isempty(stack)
+        vert = pop!(stack)
+        
+        faces = findall(==(vert), mesh.F)
+        for (_, f) in faces
+            if visited_faces[f]
+                continue
+            end
+            visited_faces[f] = true
+
+            for v in mesh.F[:, f]
+                if visited_vertices[v]
+                    continue
+                end
+                
+                push!(stack, v)
+                visited_vertices[v] = true
+            end
+        end
+    end
+
+    filter(v->visited_vertices[v], V_sampled), filter(f->skip_faces[f], F_sampled)
+end
+
+function normalized_area_mesh(mesh::Mesh)
+    total_area = sum(mesh.face_area)
+    return Mesh(mesh.V/sqrt(total_area), mesh.F, mesh.normals)
+end
 
 function spectral_decomposition(mesh::Mesh, ϕ)
     c = ϕ'*(mesh.vertex_area .* mesh.V')
